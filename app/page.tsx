c398276a22c37c3e2ae93cb176c7cf2be4d3a550
VandRTech <120173164+VandRTech@users.noreply.github.com>
2025-05-03 18:55:50 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex h-16 items-center justify-between">
             <div className="flex items-center">
-              <img src="placeholder-logo.png.png" alt="Ayuv" className="h-8 w-auto" />
+              <img src="placeholder-logo.png.png" alt="Ayuv" className="h-16 w-auto" />
               
             </div>
             <nav className="hidden md:flex items-center space-x-6">
